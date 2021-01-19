bde88cad0fac62ab58f504b8c20c74d27abcf7cd
Jana Kampfmeier <janaka@edu.aau.at>
2021-01-19 20:52:19 +0100

    login findUser

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import {MatSnackBar} from '@angular/material/snack-bar';
-
+declare var require: any;
+const db = require("./db");
+const client = db.getDb();
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
@@ -18,7 +20,34 @@ export class LoginComponent implements OnInit {
   }
 
   login(username: string, password: string) {
+
+    if(this.findUser(username,password) != null){
+      //User exists
+
+    }
+
     let message = "Username: " + username;
     this._snackBar.open(message, 'Logged In', {duration: 2000,});
+  }
+
+  //funktion die sql abfrage macht und nach dem user sucht
+  findUser(username, password){
+    //todo datenbank abfrage ob es den Nutzer gibt
+
+    let result;
+    const query = {
+      name: 'fetch-user-id',
+      text: 'SELECT id FROM user WHERE username = $1 AND password = $2',
+      values: [username, password],
+    }
+
+    client
+      .query(query)
+      .then(res => result = res.rows[0])
+      .catch(err => console.log(err.stack))
+
+    let userId = "1";
+    userId = result;
+    return userId;
   }
 }
