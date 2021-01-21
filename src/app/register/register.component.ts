1f28c905b29c383ee218f04564e404344f40501c
Jana Kampfmeier <janaka@edu.aau.at>
2021-01-21 15:37:25 +0100

    validation

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,32 +1,52 @@
-import { Component, OnInit } from '@angular/core';
-import {FormControl, FormGroup, Validators} from '@angular/forms';
+import {Component, NgModule, OnInit} from '@angular/core';
+import {AbstractControl, FormControl, FormGroup, ValidatorFn, Validators} from '@angular/forms';
 import {MatSnackBar} from '@angular/material/snack-bar';
 import {User, UserService} from '../config/user.service';
-import {Observable} from 'rxjs';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.css']
 })
+
 export class RegisterComponent implements OnInit {
 
   hide = true;
   hideconfirm = true;
-  confirmPass = new FormControl();
   errMsg: string = "";
   user: User;
 
-  //todo sonderzeichen machen probleme bei dem pattern fürs confirm passwort
+  p = new FormControl('',
+    [Validators.required, Validators.minLength(8),
+      containsNumberValidator(), containsSymbolValidator()])
 
-  constructor(private _snackBar: MatSnackBar, private userService: UserService) { }
+  confirm(first, second){
+    return first === second;
+  }
+
+  getErrorMessage() {
+    if (this.p.hasError('required')) {
+      return 'You must enter a value';
+    }
+    else if (this.p.hasError('minlength')) {
+      return 'Password min lenght is 8';
+    }
+    else if (this.p.hasError('noNumber')) {
+      return 'Password requires a number';
+    }
+    else if (this.p.hasError('noSymbol')) {
+      return 'Password requires a symbol';
+    }
+  }
+
+  constructor(private _snackBar: MatSnackBar, private userService: UserService, private route: Router) { }
 
   register(username, password, confirm){
-    this.passwordErr(password);
     if(username.length===0){
       this.printErrMsg("Username not entered");
     }
-    else if(this.passwordErrShow){
+    else if(this.p.invalid){
       this.printErrMsg("Password not valid");
     }
     else if(password != confirm){
@@ -35,6 +55,7 @@ export class RegisterComponent implements OnInit {
     else {
       // check user expect boolean return
       let exists: boolean = this.userService.checkUserExist(username);
+      exists = true;
       //wenn nutzer existiert
       if(exists) this.printErrMsg("Dieser Nutzername ist schon vergeben!");
       // wenn nicht
@@ -42,9 +63,10 @@ export class RegisterComponent implements OnInit {
         // post user expect arr with boolean and event. err msg return
         let response = this.userService.addUser(username, password);
         let added: boolean = response.added;
+        added = true;
         // wenn funktioniert
         if(added) {
-          //todo redirect zu Login
+          this.route.navigateByUrl('/login');
         }
         // wenn Fehler
         else {
@@ -53,7 +75,6 @@ export class RegisterComponent implements OnInit {
           // bleib bei register
         }
       }
-
     }
   }
 
@@ -61,36 +82,21 @@ export class RegisterComponent implements OnInit {
     this.errMsg = err;
   }
 
-  passwordErrMsg: string;
-  passwordErrShow: boolean;
-
-  passwordErr(pass: string){
-    if(pass.length<8){
-      this.passwordErrShow = true;
-      this.passwordErrMsg = "Password length needs to be min 8";
-    }
-    else if(!this.hasNumber(pass)){
-      this.passwordErrShow = true;
-      this.passwordErrMsg = "Password needs min one number";
-    }
-    else if(!this.hasSymbol(pass)){
-      this.passwordErrShow = true;
-      this.passwordErrMsg = "Password needs min one symbol";
-    }
-    else this.passwordErrShow = false;
-  }
-
-  hasNumber(pass: string){
-    return /\d/.test(pass);
-  }
-
-  private hasSymbol(pass: string) {
-    return /[\s~`!@#$%\^&*=\-\[\]\\';,/{}|\\":<>\?()\._]/g.test(pass);
-  }
-
   ngOnInit(): void {
   }
 
+}
 
+export function containsNumberValidator(): ValidatorFn {
+  return (control: AbstractControl): {[key: string]: any} | null => {
+    const forbidden = !/\d/.test(control.value);
+    return forbidden ? {noNumber: {value: control.value}} : null;
+  };
+}
 
+export function containsSymbolValidator(): ValidatorFn {
+  return (control: AbstractControl): {[key: string]: any} | null => {
+    const forbidden = !/[\s~`!@#$%\^&*=\-\[\]\\';,/{}|\\":<>\?()\._]/.test(control.value);
+    return forbidden ? {noSymbol: {value: control.value}} : null;
+  };
 }
