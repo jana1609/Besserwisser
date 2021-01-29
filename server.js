274813cda9875d6d2b2b97af0d79fe5889e70d39
Jana Kampfmeier <janaka@edu.aau.at>
2021-01-29 01:27:37 +0100

    package.json

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.use(express.static(__dirname + '/dist/besserwisser'));
 
 app.get('/*', function(req,res) {
 
-  res.sendFile(path.join(__dirname+'/dist/besserwisser/index.html'));
+  res.sendFile(path.join(__dirname+'/dist/besserwisser/src/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
