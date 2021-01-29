071244c349b53e1fd71092f67bee813a1f65f6f6
Jana Kampfmeier <janaka@edu.aau.at>
2021-01-29 01:39:23 +0100

    package.json

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('/src'));
+app.use(express.static(process.cwd()+"/dist/besserwisser/"));
 
 app.get('/*', function(req,res) {
 
-  res.sendFile(path.join('/src/index.html'));
+  res.sendFile(path.join(process.cwd()+"/dist/besserwisser/index.html"));
 });
 
 // Start the app by listening on the default Heroku port
